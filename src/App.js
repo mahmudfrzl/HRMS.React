55731a71a452d9e96b2853d36e5e0fe3b4582a92
Mahmud <mahmudfrzl@mail.ru>
2021-06-11 18:14:45 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
     </div>
   );
 }
 
-export default App;
+export default App; 
